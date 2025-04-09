5adda79eea65d81ef8a0df14f0b0472b070cc3f2
ArturoCarrilloJimenez <arturojimenezalmontaras@gmail.com>
2025-04-09 11:56:43 +0200

    refactor: add environments  production

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  baseUrl: 'https://sylehub-back.onrender.com/api/v1/',
+  title: 'StyleHub',
+};
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: process.env['BASE_URL_API'],
-  title: process.env['TITLE'],
+  baseUrl: 'http://localhost:3000/api/v1/',
+  title: 'StyleHub',
 };
